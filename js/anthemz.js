cb7f2a5c11d50c384c061aa26bcdf7ddb0182ac5
wusgitaccount <swu18@mywhitecliffe.com>
2020-05-04 14:33:46 +1200

    Adding video stills

---
+++ js/anthemz.js
@@ -0,0 +1,16 @@
+// <script type="text/javascript">
+// $(document).ready(function() {
+//   var videoID = 'videoclip';
+//   var sourceID = 'mp4video';
+//   var newmp4 = './assets/videocallz.mp4';
+//
+//
+//   $('#videolink1').click(function(event) {
+//     $('#'+videoID).get(0).pause();
+//     $('#'+sourceID).attr('src', newmp4);
+//     $('#'+videoID).get(0).load();
+//      //$('#'+videoID).attr('poster', newposter); //Change video poster
+//     $('#'+videoID).get(0).play();
+//   });
+// });
+// </script>
